4cde4af31b20a7d657da629f4e24964741737afc
allx-under <95172339+allx-under@users.noreply.github.com>
2022-05-16 13:56:37 +0300

    fix task1

--- js/task-01.js
+++ js/task-01.js
@@ -2,20 +2,18 @@ const categories = document.querySelector("#categories");
 
 const categsAmount = categories.children.length;
 
-console.log(`Number of categories: ${categsAmount}`);
-
 const categItemsEl = [...categories.children];
 
-
-
 function checkCategAndElements() {
-    
+    console.log(`Number of categories: ${categsAmount}`);
+
     categItemsEl.forEach(category => {
         const categsTitle = category.firstElementChild.textContent;
     const categsAmountEl = category.lastElementChild.children.length;
         console.log(`Category: ${categsTitle}
 Elements: ${categsAmountEl}`)
     });
+    
 }
 
 checkCategAndElements()
